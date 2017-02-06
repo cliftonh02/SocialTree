d72194fc6ad73842891e14d198a7519d9f161942
Frontlines 3 <cliftonh02@gmail.com>
2017-02-06 11:44:49 -0500

    Second Commit

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ var mongoose = require("mongoose");
 
 var app = express();
 
-app.listen('3000', function(){
-  console.log("Listening for localhost 3000")
+var mongoose.connect('mongodb://localhost:27017/SocialTree');
+
+app.get('/', function(req, res){
+  res.send('index.html');
 })
+
+app.listen('3000', function(){
+  console.log("Listening for localhost 3000");
+});
