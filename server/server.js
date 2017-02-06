8d1e43628b5354665d0448357013f57cbdbb86fc
Frontlines 3 <cliftonh02@gmail.com>
2017-02-06 13:50:07 -0500

    sixth commit added css folder

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 var express = require("express");
-var bodParser = require("body-parser");
 var mongoose = require("mongoose");
+var bodParser = require("body-parser");
 
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/SocialTree');
 
-app.use('/app', express.static(__dirname, + "/app"));
-app.use('node_modules', express.static(__dirname + "/node_modules"));
+app.use('/app', express.static(__dirname + "/app"));
+app.use('/node_modules', express.static(__dirname + "/node_modules"));
 
 app.get('/', function(req, res){
   res.send('index.html');
-})
+});
 
 app.listen('3000', function(){
   console.log("Listening for localhost 3000");
